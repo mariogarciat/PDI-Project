552b381f0e1981236e5e072079b8aea196c8e485
danielospinap <daniel.ospinaperez@gmail.com>
2018-05-29 12:09:05 -0500

    nuevas mascaras video2

--- velocidad_vehiculos.m
+++ velocidad_vehiculos.m
@@ -1,38 +1,55 @@
 close all, clear all, clc
 
-video = VideoReader('video vehiculos.mp4'); % Abre el archivo de video
+video = VideoReader('video2.mp4'); % Abre el archivo de video
 
-% rowSize = video.Height; 
-% space = rowSize*0.05;
 numFrames = get(video,'NumberOfFrames'); % Obtiene el numero de frames del video
 shot = read(video,1); % Toma un frame de referencia
-shot = imrotate(shot, -90); % Rota el frame
+
+figure(1); imshow (shot); impixelinfo;
 
 
 % Define los umbrales de tolerancia a partir de los cuales se considera que
 % el cambio es por un vehiculo
-umbralSuperior = 60000;
-umbralInferior = 250000;
+umbralizquierda = 60000;
+umbralderecha = 250000;
 
 
 [fil,col,cap] = size(shot); %Toma las medidas del frame
 
-maskDown = shot * 0; % Crea en negro la mascara inferior
-maskUp = shot * 0; % Crea en negro la mascara superior
 
-% maskDown(953:973, 885:1090, :) = 150;
-% maskUp(410:430, 910:1000, :) = 150;
 
-maskDown(1010:1050, 200:550, :) = 150; % Define el area a analizar de la parte inferior del frame
-maskUp(130:150, 310:410, :) = 150; % Define el area a analizar de la parte superior del frame
+% CREACION DE LAS MASCARAS
 
-indDown = find(maskDown ~= 0); % Obtiene los indices del area a analizar
-indUp = find(maskUp ~= 0); % Obtiene los indices del area a analizar
+maskDerArriba = shot * 0; % Crea en negro la mascara derecha del carril superior
+maskIzqArriba = shot * 0; % Crea en negro la mascara izquierda del carril superior
 
-maskDownActual = maskDown; % Crea una mascara para analizar el frame actual
-maskUpActual = maskUp; % Crea una mascara para analizar el frame actual
-maskDownSiguiente = maskDown; % Crea una mascara para analizar el frame siguiente
-maskUpSiguiente = maskUp; % Crea una mascara para analizar el frame siguiente
+maskDerAbajo = shot * 0; % Crea en negro la mascara derecha del carril inferior
+maskIzqAbajo = shot * 0; % Crea en negro la mascara izquierda del carril inferior
+
+maskDerArriba(480:630, 1800:1840, :) = 150; % Define el area a analizar de la parte derecha del frame del carril superior
+maskIzqArriba(480:630, 90:130, :) = 150; % Define el area a analizar de la parte izquierda del frame del carril superior
+
+maskDerAbajo(690:850, 1800:1840, :) = 150; % Define el area a analizar de la parte derecha del frame del carril inferior
+maskIzqAbajo(690:850, 90:130, :) = 150; % Define el area a analizar de la parte izquierda del frame del carril inferior
+
+
+% TOMA DE INDICES DE LAS MASCARAS
+indDerArriba = find(maskDerArriba ~= 0);
+indIzqArriba = find(maskIzqArriba ~= 0);
+indDerAbajo = find(maskDerAbajo ~= 0);
+indIzqAbajo = find(maskIzqAbajo ~= 0); 
+
+% CREA COPIAS DE LAS MASCARAS PARA DOS FRAMES A COMPARAR 
+% (ACTUAL Y SIGUIENTE)
+maskDerArribaActual = maskDerArriba;
+maskIzqArribaActual = maskIzqArriba;
+maskDerAbajoActual = maskDerAbajo;
+maskIzqAbajoActual = maskIzqAbajo;
+
+maskDerArribaSiguiente = maskDerArriba;
+maskIzqArribaSiguiente = maskIzqArriba;
+maskDerAbajoSiguiente = maskDerAbajo;
+maskIzqAbajoSiguiente = maskIzqAbajo;
 
 % myfotog = read(video, 401);
 % myfotog = imrotate(myfotog, -90);
@@ -40,80 +57,89 @@ maskUpSiguiente = maskUp; % Crea una mascara para analizar el frame siguiente
 % figure(9); imshow(maskUp); impixelinfo;
 
 
-frameId = 4964; % Frame de inicio del video
+frameId = 1; % Frame de inicio del video
 
-entradas = 0; % Contador de autos que entran al area de inspeccion
-vehiculoPasandoUp = 0; % Bandera para saber si un auto esta pasando por la mascara superior
-vehiculoPasandoDown = 0; % Bandera para saber si un auto esta pasando por la mascara inferior
+entradasCarrilSuperior = 0; % Contador de autos que entran en el carril superior
+entradasCarrilInferior = 0; % Contador de autos que entran en el carril inferior
+
+vehiculoPasandoDerArriba = 0; % Bandera para saber si un auto esta pasando por la mascara derecha del carril superior
+vehiculoPasandoIzqArriba = 0; % Bandera para saber si un auto esta pasando por la mascara izquierda del carril superior
+vehiculoPasandoDerAbajo = 0; % Bandera para saber si un auto esta pasando por la mascara derecha del carril inferior
+vehiculoPasandoIzqAbajo = 0; % Bandera para saber si un auto esta pasando por la mascara izquierda del carril inferior
+
+
 
 % Ciclo para recorrer todos los frames del video y analizarlos
 while (frameId < numFrames) 
     frameActual = read(video, frameId); % Lee un frame
-    frameActual = imrotate(frameActual, -90); % Gira el frame
-    frameSiguiente = read(video, frameId+2); % Lee un segundo frame dejando uno de por medio para notar el cambio
-    frameSiguiente = imrotate(frameSiguiente, -90); % Gira el frame
+    frameSiguiente = read(video, frameId+1); % Lee un segundo frame para comparar
+        
+    % Llena las mascaras de la izquierda de ambos carriles con los datos de
+    % los dos frames
+    maskIzqArribaActual(indIzqArriba) = frameActual(indIzqArriba);
+    maskIzqAbajoActual(indIzqAbajo) = frameActual(indIzqAbajo);
+    maskIzqArribaSiguiente(indIzqArriba) = frameSiguiente(indIzqArriba);
+    maskIzqAbajoSiguiente(indIzqAbajo) = frameSiguiente(indIzqAbajo);
     
-    % Llena las mascaras superiores con los datos de los dos frames frames
-    maskUpActual(indUp) = frameActual(indUp);
-    maskUpSiguiente(indUp) = frameSiguiente(indUp);
+    figure(2); imshow ([maskIzqArribaActual; maskIzqArribaSiguiente]); impixelinfo;
     
-    % Realiza la resta entre ambas mascaras para denotar el cambio
-    diferenciaUp = maskUpActual - maskUpSiguiente;
+    % calcula la diferencia entre las mascaras de los dos frames para ambos carriles
+    diferenciaArribaIzq = maskIzqArribaActual - maskIzqArribaSiguiente;
     % Suma todos los valores de la imagen que cambiaron y los reduce a un solo numero
-    diferenciaUp = sum(sum(sum(diferenciaUp)));
+    diferenciaArribaIzq = sum(sum(sum(diferenciaArribaIzq)));
     
-    entradas
-    diferenciaUp
+%     entradasCarrilSuperior
+    diferenciaArribaIzq
     
     % Verifica si el cambio supera el umbral y la bandera para saber si ya
     % habia iniciado un cambio debido a un vehiculo
-    if(diferenciaUp > umbralSuperior && vehiculoPasandoUp == 0)
-        entradas = entradas + 1; % Suma uno al contador de vehiculos que entran en el area de analisis
-        vehiculoPasandoUp = 1; % Cambia la bandera para no seguir sumando vehiculos sin necesidad
-        
+% % %     if(diferenciaUp > umbralizquierda && vehiculoPasandoUp == 0)
+% % %         entradas = entradas + 1; % Suma uno al contador de vehiculos que entran en el area de analisis
+% % %         vehiculoPasandoUp = 1; % Cambia la bandera para no seguir sumando vehiculos sin necesidad
+% % %         
     % Si no se supera el umbral y si ya habia un vehiculo sobre la mascara
     % se cambia la bandera
-    elseif(diferenciaUp <= umbralSuperior && vehiculoPasandoUp == 1)
-        vehiculoPasandoUp = 0;
-    end
+% % %     elseif(diferenciaUp <= umbralizquierda && vehiculoPasandoUp == 1)
+% % %         vehiculoPasandoUp = 0;
+% % %     end
     
-    % Se inicializa la variable del cambio de la mascara inferior
-    diferenciaDown = 0;
+    % Se inicializa la variable del cambio de la mascara derecha
+% % %     diferenciaDown = 0;
     
     % Verifica si hay vehiculos en el area para analizarlos
-    if(entradas > 0)
-        % Llena las mascaras inferiores con los datos de los dos frames frames
-        maskDownActual(indDown) = frameActual(indDown);
-        maskDownSiguiente(indDown) = frameSiguiente(indDown);
-        
-        % Realiza la resta entre ambas mascaras para denotar el cambio
-        diferenciaDown = maskDownActual - maskDownSiguiente;
-        % Suma todos los valores de la imagen que cambiaron y los reduce a un solo numero
-        diferenciaDown = sum(sum(sum(diferenciaDown)));
-        
-        % Verifica si el cambio supera el umbral y la bandera para saber si ya
-        % habia iniciado un cambio debido a un vehiculo
-        if(diferenciaDown > umbralInferior && vehiculoPasandoDown == 0)
-            % Disminuye el contador debido a que salio un vehiculo del area
-            entradas = entradas - 1;
-            % Cambia el valor de la bandera
-            vehiculoPasandoDown = 1;
-            
-        % Si no se supera el umbral y si ya habia un vehiculo sobre la mascara
-        % se cambia la bandera
-        elseif(diferenciaDown <= umbralInferior && vehiculoPasandoDown == 1)
-            vehiculoPasandoDown = 0;
-        end
-    
-    % Si no hay vehiculos en el area se desactiva la bandera
-    elseif(entradas == 0)
-        vehiculoPasandoDown = 0;
-    end
-    
-    
-    if(diferenciaUp > umbralSuperior || diferenciaDown > umbralInferior)
-        figure(7); imshow(frameActual); impixelinfo;
-    end
+% % %     if(entradas > 0)
+% % %         % Llena las mascaras derechaes con los datos de los dos frames frames
+% % %         maskDownActual(indDown) = frameActual(indDown);
+% % %         maskDownSiguiente(indDown) = frameSiguiente(indDown);
+% % %         
+% % %         % Realiza la resta entre ambas mascaras para denotar el cambio
+% % %         diferenciaDown = maskDownActual - maskDownSiguiente;
+% % %         % Suma todos los valores de la imagen que cambiaron y los reduce a un solo numero
+% % %         diferenciaDown = sum(sum(sum(diferenciaDown)));
+% % %         
+% % %         % Verifica si el cambio supera el umbral y la bandera para saber si ya
+% % %         % habia iniciado un cambio debido a un vehiculo
+% % %         if(diferenciaDown > umbralderecha && vehiculoPasandoDown == 0)
+% % %             % Disminuye el contador debido a que salio un vehiculo del area
+% % %             entradas = entradas - 1;
+% % %             % Cambia el valor de la bandera
+% % %             vehiculoPasandoDown = 1;
+% % %             
+% % %         % Si no se supera el umbral y si ya habia un vehiculo sobre la mascara
+% % %         % se cambia la bandera
+% % %         elseif(diferenciaDown <= umbralderecha && vehiculoPasandoDown == 1)
+% % %             vehiculoPasandoDown = 0;
+% % %         end
+% % %     
+% % %     % Si no hay vehiculos en el area se desactiva la bandera
+% % %     elseif(entradas == 0)
+% % %         vehiculoPasandoDown = 0;
+% % %     end
+% % %     
+% % %     
+% % %     if(diferenciaUp > umbralizquierda || diferenciaDown > umbralderecha)
+% % %         figure(7); imshow(frameActual); impixelinfo;
+% % %     end
     
 %     x = sum(sum(sum(diferenciaDown)));
 %     if(x > 150000)
@@ -131,17 +157,17 @@ while (frameId < numFrames)
 %         mysum = 0;
 %     end
     
-    % Para la superior un cambio mayor a 35000 se considera un vehiculo
+    % Para la izquierda un cambio mayor a 35000 se considera un vehiculo
 %     sum(sum(sum(diferenciaDown)))
     
 %     figure(1); imshow(diferenciaDown);
 %     figure(2); imshow(diferenciaDown);
 %     figure(3); imshow(frameActual-frameSiguiente);
 %     figure(4); imshow([diferenciaDown diferenciaUp]); impixelinfo;
-    vehiculoPasandoDown
-    vehiculoPasandoUp
-    
-    % Itera el numero del frame
+% % %     vehiculoPasandoDown
+% % %     vehiculoPasandoUp
+% % %     
+% % %     % Itera el numero del frame
     frameId = frameId + 1
 end
 
